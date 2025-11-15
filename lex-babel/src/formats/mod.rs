2a76f9828abf0fc18ca90a646c2b3d360cd0a3ee
adebert <debert@gmail.com>
2025-11-15 19:17:02 -0300

    Removing ai deluge

--- lex-babel/src/formats/mod.rs
+++ lex-babel/src/formats/mod.rs
@@ -3,15 +3,10 @@
 //! This module contains all format implementations that convert between
 //! Lex AST and various text representations.
 
-pub mod html;
 pub mod lex;
-pub mod markdown;
-pub mod pandoc;
 pub mod tag;
 pub mod treeviz;
 
-pub use html::HtmlFormat;
 pub use lex::LexFormat;
-pub use pandoc::PandocJsonFormat;
 pub use tag::TagFormat;
 pub use treeviz::TreevizFormat;
