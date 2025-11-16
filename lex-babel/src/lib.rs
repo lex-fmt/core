6c6a950978dc614abc2dd038cff1548619f9ddc0
adebert <debert@gmail.com>
2025-11-16 19:52:47 -0300

    Lex-babel: improved cargo docs

--- lex-babel/src/lib.rs
+++ lex-babel/src/lib.rs
@@ -5,15 +5,13 @@
 //!
 //! Architecture
 //!
-//!     - Format trait: Uniform interface for all formats (parsing and/or serialization)
-//!     - FormatRegistry: Centralized discovery and selection of formats
-//!     - Format implementations: Concrete implementations for each supported format
+//!     The goal here is to, as much as possible, split what is the common logic for multiple formats
+//!     conversions into a format agnoistic layer. This is done by the using the IR representation (./ir/mod.rs),
+//!     and having the common code in ./mappings/mod.rs. This allows for the format specific code to be focused on the data format transformations, while having a strong, focused core that can be well tested in isolation.
 //!
 //!     This is a pure lib, that is , it powers the lex-cli but is shell agnostic, that is no code
 //!     should be written that supposes a shell environment, be it to std print, env vars etc.
-//!
-//!     Format specific capabilities are implemented with the Format trait. formats should have a
-//!     parse() and serialize() method, a name and file extensions. See the trait def [./format.rs ]
+
 //!
 //!     The file structure :
 //!     .
@@ -27,6 +25,8 @@
 //!     │   │   └── mod.rs
 //!     |   ├─  interop             # Shared conversion utilities
 //!     ├── lib.rs
+//!     ├── ir                      # Intermediate Representation
+//!     ├── mappings                # Common mapping code
 //!
 //! Testing   
 //!     tests
@@ -38,8 +38,26 @@
 //!         ├── kitchensink.lex
 //!         └── kitchensink.md
 //!
-//!     note that rust does not by default discover tests in subdirectories, so we need to include these
+//!     Note that rust does not by default discover tests in subdirectories, so we need to include these
 //!     in the mod.
+//!
+//!
+//! Core Algorithms
+//!
+//!     The most complex part of the work is reconstructing a nested representation from a flat document, followed by the reverse operations.  For this reason we have a common IR (./ir/mod.rs) that is used for all formats.
+//!     Over this representation we implement both algorithms (see ./mappings/flat_to_nested.rs and ./mappings/nested_to_flat.rs).
+//!     This means that all the heavy lifting is done by a core, well tested and maintained module,
+//! freeing format adaptations to be focused on the simpler data format transformations.
+//!
+//!
+//! Formats
+//!
+//!     Format specific capabilities are implemented with the Format trait. formats should have a
+//!     parse() and serialize() method, a name and file extensions. See the trait def [./format.rs ]
+//!     - Format trait: Uniform interface for all formats (parsing and/or serialization)
+//!     - FormatRegistry: Centralized discovery and selection of formats
+//!     - Format implementations: Concrete implementations for each supported format
+//!
 //!
 //! The Lex Format
 //!
@@ -52,14 +70,6 @@
 //!
 //!     This means that full format interop round tripping is not possible.
 //!
-//!
-//! Library Choices
-//!
-//!     This, not being lex's core means that we will offload as much as possible to better, specialized creates for each format. the escope here is mainly to adapt the ast's from lex to the format or vice versa. For example we never write the serializer for , say markdown, but pass the AST to the mardown library.
-//!     To support a format inbound, we write the format ast -> lex ast adapter. likewise, for outbound formats we will do the reverse, converting from the lex ast to the format's.
-//!
-//!     As much as possible, we will use rust crates, and avoid shelling out and having outside dependencies.
-//!
 //! Format Selection
 //!
 //!     The choice for the formats is pretty sensible:
@@ -68,16 +78,26 @@
 //!     - Markdown: both in and to, as Mardown is the universal format for plain text editing.
 //!     - XML: serializing Lex's is trivial and can be useful as a structured format for storage.
 //!
-//!     These are table stakes, that is a format that can't export to HTML, convert to markdown or lack a good semantic pure xml output is a non starter.
+//!     These are table stakes, that is a format that can't export to HTML, convert to markdown or
+//! lack a good semantic pure xml output is a non starter.
 //!
 //!
-//!     For everything else, there is good arguments for a variety of formats. The one that has the strongest fit and use case is Latex, as Lex can be very useful for scientific writing. But latex is complicated, and having pandoc in the pipeline allows us to serve reasonably well pretty much any other format.
+//!     For everything else, there is good arguments for a variety of formats. The one that has the strongest fit
+//!  and use case is Latex, as Lex can be very useful for scientific writing. But latex is
+//!  complicated, and having pandoc in the pipeline allows us to serve reasonably well pretty much
+//!  any other format.
 //!
-//! Core Algorithms
+//! Library Choices
 //!
-//!     The most complex part of the work is reconstructing a nested representation from a flat document, followed by the reverse operations.  For this reason we have a common IR (./ir/mod.rs) that is used for all formats.
-//!     Over this representation we implement both algorithms (see ./mappings/flat_to_nested.rs and ./mappings/nested_to_flat.rs).
-//!     This means that all the heavy lifting is done by a core, well tested and maintained module, freeing format adaptations to be focused on the simpler data format transformations.
+//!     This, not being lex's core means that we will offload as much as possible to better, specialized creates
+//!  for each format. the escope here is mainly to adapt the ast's from lex to the format or vice
+//!  versa. For example we never write the serializer for , say markdown, but pass the AST to the
+//!     mardown library. To support a format inbound, we write the format ast -> lex ast adapter.
+//!  likewise, for outbound formats we will do the reverse, converting from the lex ast to the
+//!  format's.
+//!
+//!     As much as possible, we will use rust crates, and avoid shelling out and having outside dependencies.
+//!
 pub mod error;
 pub mod format;
 pub mod formats;
