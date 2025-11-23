8894a53eef6fcd74a0612d317c8c4f365be4a535
adebert <debert@gmail.com>
2025-11-22 21:48:38 -0300

    test(vscode-m5): cover hover responses

--- editors/vscode/test/integration/helpers.ts
+++ editors/vscode/test/integration/helpers.ts
@@ -3,6 +3,29 @@ import * as vscode from 'vscode';
 
 export const TEST_DOCUMENT_PATH = 'documents/getting-started.lex';
 export const SEMANTIC_TOKENS_DOCUMENT_PATH = 'documents/semantic-tokens.lex';
+export const HOVER_DOCUMENT_PATH = 'documents/semantic-tokens.lex';
+
+export interface PositionMatch {
+  line: number;
+  character: number;
+}
+
+export function findPosition(
+  document: vscode.TextDocument,
+  searchText: string
+): PositionMatch | undefined {
+  const text = document.getText();
+  const index = text.indexOf(searchText);
+  if (index === -1) {
+    return undefined;
+  }
+
+  const prefix = text.slice(0, index);
+  const line = (prefix.match(/\n/g) || []).length;
+  const lastLineBreak = prefix.lastIndexOf('\n');
+  const character = index - (lastLineBreak + 1);
+  return { line, character };
+}
 
 export function requireWorkspaceFolder(): vscode.WorkspaceFolder {
   const folder = vscode.workspace.workspaceFolders?.[0];
